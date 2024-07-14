caa5033a70b8b380d9b08caeaf2d1fee84653c39
omatheusfeitosa <matheusfeitosaalcantara@gmail.com>
2024-07-14 14:35:06 -0300

    Adicionei animacoes

---
+++ js/app.js
@@ -0,0 +1,26 @@
+ScrollReveal().reveal('header, .secao-principal', {
+    origin: 'left',
+    duration: 2000,
+    distance: '20%',
+})
+
+ScrollReveal().reveal('.conteudo-secundario, .imagem1, .imagem2', {
+    origin: 'left',
+    duration: 2000,
+    distance: '20%',
+})
+
+ScrollReveal().reveal('.secao-terciaria h2, .secao-terciaria p, .imagem3, .imagem4, .imagem5', {
+    origin: 'left',
+    duration: 2000,
+    distance: '20%',
+})
+
+ScrollReveal().reveal('.secao-depoimento-principal, .depoiemnto1, .depoimento2, .depoimento3', {
+    origin: 'left',
+    duration: 2000,
+    distance: '20%',
+})
+
+
+
